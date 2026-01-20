3c02aa371749953125b787a94298eca9d5c27d37
travis holt <trvslhlt@gmail.com>
2026-01-19 20:09:48 -0500

    setup project

---
+++ src/ift6135/__init__.py
@@ -0,0 +1,15 @@
+"""IFT 6135 Representation Learning Course Utilities."""
+
+from ift6135.setup import (
+    is_colab,
+    get_repo_root,
+    setup_environment,
+    get_data_path,
+)
+
+__all__ = [
+    "is_colab",
+    "get_repo_root",
+    "setup_environment",
+    "get_data_path",
+]
